2327084e20abfb85931065adc41031bd3800c7ab
arturusfury <bri.reynolds@gmail.com>
2016-09-27 11:05:14 -0500

    added a blank race to test with in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# == RACE ===========================
+# t.string :race_name
+# t.text :description
+# t.string :stats
+# t.string :age_description
+# t.integer :age_min
+# t.integer :age_max
+# t.string :size_description
+# t.string :size
+# t.integer :min_height
+# t.integer :max_height
+# t.integer :min_weight
+# t.integer :max_weight
+# t.integer :speed
+# t.string :misc
+Race.new(
+  race_name: '',
+  description: '',
+  stats: [0, 0, 0, 0, 0, 0],
+  age_description: '',
+  age_min: 17,
+  age_max: 100,
+  size_description: '',
+  size: 'medium',
+  min_height: 75,
+  max_height: 150,
+  min_weight: 100,
+  max_weight: 300,
+  speed: 30,
+  misc: ''
+)
