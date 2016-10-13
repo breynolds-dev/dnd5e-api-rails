f4c80811bef8020351eb5339382d500687c02c09
arturusfury <bri.reynolds@gmail.com>
2016-10-13 15:30:52 -0500

    updaded seeds to finish adding languages

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,14 +62,114 @@
 # == LANGUAGES ===========================
 # t.string :name
 # t.string :script
+@abyssal = Language.create(
+  name: 'Abyssal',
+  script: 'Infernal'
+)
+
+@aquan = Language.create(
+  name: 'Aquan',
+  script: 'Elvish'
+)
+
+@auran = Language.create(
+  name: 'Auran',
+  script: 'Draconic'
+)
+
+@celestial = Language.create(
+  name: 'Celestial',
+  script: 'Celestial'
+)
+
 @common = Language.create(
   name: 'Common',
   script: 'Common'
 )
 
+@deep_speech = Language.create(
+  name: 'Deep Speech',
+  script: ''
+)
+
+@draconic = Language.create(
+  name: 'Draconic',
+  script: 'Draconic'
+)
+
+@druidic = Language.create(
+  name: 'Druidic',
+  script: 'Druidic'
+)
+
+@dwarvish = Language.create(
+  name: 'Dwarvish',
+  script: 'Dwarvish'
+)
+
+@elvish = Language.create(
+  name: 'Elvish',
+  script: 'Elvish'
+)
+
+@giant = Language.create(
+  name: 'Giant',
+  script: 'Dwarvish'
+)
+
 @gnomish = Language.create(
   name: 'Gnomish',
   script: 'Dwarvish'
+)
+
+@goblin = Language.create(
+  name: 'Goblin',
+  script: 'Dwarvish'
+)
+
+@gnoll = Language.create(
+  name: 'Gnoll',
+  script: 'Common'
+)
+
+@halfling = Language.create(
+  name: 'Halfling',
+  script: 'Common'
+)
+
+@ignan = Language.create(
+  name: 'Ignan',
+  script: 'Draconic'
+)
+
+@infernal = Language.create(
+  name: 'Infernal',
+  script: 'Infernal'
+)
+
+@orc = Language.create(
+  name: 'Orc',
+  script: 'Dwarvish'
+)
+
+@primordial = Language.create(
+  name: 'Primordial',
+  script: 'Dwarvish'
+)
+
+@sylvan = Language.create(
+  name: 'Sylvan',
+  script: 'Elvish'
+)
+
+@terran = Language.create(
+  name: 'Terran',
+  script: 'Dwarvish'
+)
+
+@undercommon = Language.create(
+  name: 'Undercommon',
+  script: 'Elvish'
 )
 
 # == JOINS ==============================
