1b8a84ecd4dda706c78c246ecd59aa6af64ed581
arturusfury <bri.reynolds@gmail.com>
2016-12-03 08:59:46 -0600

    added a full set of levels for path of the berserker

--- db/seeds/barbarians.rb
+++ db/seeds/barbarians.rb
@@ -29,3 +29,131 @@ Barbarian.create(
   rage_count: 3,
   rage_damage_bonus: 2
 )
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 5,
+  prof_bonus: 3,
+  rage_count: 3,
+  rage_damage_bonus: 2
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 6,
+  prof_bonus: 3,
+  rage_count: 4,
+  rage_damage_bonus: 2
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 7,
+  prof_bonus: 3,
+  rage_count: 4,
+  rage_damage_bonus: 2
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 8,
+  prof_bonus: 3,
+  rage_count: 4,
+  rage_damage_bonus: 2
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 9,
+  prof_bonus: 4,
+  rage_count: 4,
+  rage_damage_bonus: 3
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 10,
+  prof_bonus: 4,
+  rage_count: 4,
+  rage_damage_bonus: 3
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 11,
+  prof_bonus: 4,
+  rage_count: 4,
+  rage_damage_bonus: 4
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 12,
+  prof_bonus: 4,
+  rage_count: 5,
+  rage_damage_bonus: 3
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 13,
+  prof_bonus: 5,
+  rage_count: 5,
+  rage_damage_bonus: 3
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 14,
+  prof_bonus: 5,
+  rage_count: 5,
+  rage_damage_bonus: 3
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 15,
+  prof_bonus: 5,
+  rage_count: 5,
+  rage_damage_bonus: 3
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 16,
+  prof_bonus: 5,
+  rage_count: 5,
+  rage_damage_bonus: 4
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 17,
+  prof_bonus: 6,
+  rage_count: 6,
+  rage_damage_bonus: 4
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 18,
+  prof_bonus: 6,
+  rage_count: 6,
+  rage_damage_bonus: 4
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 19,
+  prof_bonus: 6,
+  rage_count: 6,
+  rage_damage_bonus: 4
+)
+
+Barbarian.create(
+  path: 'Berserker',
+  level: 20,
+  prof_bonus: 6,
+  rage_count: 0, # convert to "Unlimited via Serializer"
+  rage_damage_bonus: 2
+)
