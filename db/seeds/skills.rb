4b708f155cd208cabce1d71874108232fc4216db
arturusfury <bri.reynolds@gmail.com>
2016-12-03 09:21:38 -0600

    updated the skills with a description

--- db/seeds/skills.rb
+++ db/seeds/skills.rb
@@ -1,90 +1,108 @@
 # Create Skills Table
 Skill.create(
   name: 'Acrobatics',
-  ability: 'Dexterity'
+  ability: 'Dexterity',
+  description: ''
 )
 
 Skill.create(
   name: 'Animal Handling',
-  ability: 'Wisdom'
+  ability: 'Wisdom',
+  description: ''
 )
 
 Skill.create(
   name: 'Arcana',
-  ability: 'Intelligence'
+  ability: 'Intelligence',
+  description: ''
 )
 
 Skill.create(
   name: 'Athletics',
-  ability: 'Strength'
+  ability: 'Strength',
+  description: 'Your Athletics check covers difficult situations you encounter while climbing, jumping, or swimming. Exam ples include the following activities:\n\rYou attempt to climb a sheer or slippery cliff, avoid hazards while scaling a wall, or cling to a surface while som ething is trying to knock you off.\n\rYou try to jum p an unusually long distance or pull off a stunt midjump.\n\rYou struggle to swim or stay afloat in treacherous currents, storm -tossed waves, or areas o f thick seaweed.\n\rOr another creature tries to push or pull you underwater or otherwise interfere with your swimming.\n\r'
 )
 
 Skill.create(
   name: 'Deception',
-  ability: 'Charisma'
+  ability: 'Charisma',
+  description: ''
 )
 
 Skill.create(
   name: 'History',
-  ability: 'Intelligence'
+  ability: 'Intelligence',
+  description: ''
 )
 
 Skill.create(
   name: 'Insight',
-  ability: 'Wisdom'
+  ability: 'Wisdom',
+  description: ''
 )
 
 Skill.create(
   name: 'Intimidation',
-  ability: 'Charisma'
+  ability: 'Charisma',
+  description: ''
 )
 
 Skill.create(
   name: 'Investigation',
-  ability: 'Intelligence'
+  ability: 'Intelligence',
+  description: ''
 )
 
 Skill.create(
   name: 'Medicine',
-  ability: 'Wisdom'
+  ability: 'Wisdom',
+  description: ''
 )
 
 Skill.create(
   name: 'Nature',
-  ability: 'Intelligence'
+  ability: 'Intelligence',
+  description: ''
 )
 
 Skill.create(
   name: 'Perception',
-  ability: 'Wisdom'
+  ability: 'Wisdom',
+  description: ''
 )
 
 Skill.create(
   name: 'Performance',
-  ability: 'Charisma'
+  ability: 'Charisma',
+  description: ''
 )
 
 Skill.create(
   name: 'Persuasion',
-  ability: 'Charisma'
+  ability: 'Charisma',
+  description: ''
 )
 
 Skill.create(
   name: 'Religion',
-  ability: 'Intelligence'
+  ability: 'Intelligence',
+  description: ''
 )
 
 Skill.create(
   name: 'Sleight of Hand',
-  ability: 'Dexterity'
+  ability: 'Dexterity',
+  description: ''
 )
 
 Skill.create(
   name: 'Stealth',
-  ability: 'Dexterity'
+  ability: 'Dexterity',
+  description: ''
 )
 
 Skill.create(
   name: 'Survival',
-  ability: 'Wisdom'
+  ability: 'Wisdom',
+  description: ''
 )
