de5ff901fce9e16e86dbb2f2d2b5900a2942eef3
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-12-02 22:47:53 -0600

    add barbarian table

---
+++ db/migrate/20161203033956_create_barbarians.rb
@@ -0,0 +1,11 @@
+class CreateBarbarians < ActiveRecord::Migration[5.0]
+  def change
+    create_table :barbarians do |t|
+      t.string  :subclass
+      t.integer :level
+      t.integer :prof_bonus
+      t.integer :rage_count
+      t.integer :rage_damage
+    end
+  end
+end
