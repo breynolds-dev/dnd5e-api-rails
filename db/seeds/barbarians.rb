36818dcd5558829a7fc1b875799a76ccf2f357cd
arturusfury <bri.reynolds@gmail.com>
2016-12-02 23:42:29 -0600

    added a few more levels into our seed file for db testing

--- db/seeds/barbarians.rb
+++ db/seeds/barbarians.rb
@@ -1,7 +1,31 @@
 Barbarian.create(
   subclass: 'Barbarian',
   level: 1,
-  prof_bonus: 1,
+  prof_bonus: 2,
   rage_count: 1,
-  rage_damage_bonus: 1
+  rage_damage_bonus: 2
+)
+
+Barbarian.create(
+  subclass: 'Barbarian',
+  level: 2,
+  prof_bonus: 2,
+  rage_count: 2,
+  rage_damage_bonus: 2
+)
+
+Barbarian.create(
+  subclass: 'Barbarian',
+  level: 3,
+  prof_bonus: 2,
+  rage_count: 3,
+  rage_damage_bonus: 2
+)
+
+Barbarian.create(
+  subclass: 'Barbarian',
+  level: 4,
+  prof_bonus: 2,
+  rage_count: 3,
+  rage_damage_bonus: 2
 )
