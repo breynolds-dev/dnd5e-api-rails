1b6668516b5fb0ea8439afa43272ca118c61520c
arturusfury <bri.reynolds@gmail.com>
2016-12-07 10:55:16 -0600

    created our warlock basic table

--- db/migrate/20161204001112_create_warlocks.rb
+++ db/migrate/20161204001112_create_warlocks.rb
@@ -1,8 +1,14 @@
 class CreateWarlocks < ActiveRecord::Migration[5.0]
   def change
     create_table :warlocks do |t|
-
-      t.timestamps
+      t.string :pact
+      t.integer :level
+      t.integer :prof_bonus
+      t.integer :cantrips_known
+      t.integer :spells_known
+      t.integer :spell_slots
+      t.integer :slot_level
+      t.integer :invocations_known
     end
   end
 end
