1d1043edaf41a998e98457932eb751c275f8fbb7
arturusfury <bri.reynolds@gmail.com>
2016-12-07 11:57:13 -0600

    added seed data for monks

---
+++ db/seeds/class/monks.rb
@@ -0,0 +1,345 @@
+Monk.create(
+  tradition: 'Monk',
+  level: 1,
+  prof_bonus: 2,
+  martial_arts: 4,
+  ki_points: 0,
+  unarmored_movement: 0
+)
+
+Monk.create(
+  tradition: 'Monk',
+  level: 2,
+  prof_bonus: 2,
+  martial_arts: 4,
+  ki_points: 2,
+  unarmored_movement: 10
+)
+
+# == WAY OF THE OPEN HAND =================
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 3,
+  prof_bonus: 2,
+  martial_arts: 4,
+  ki_points: 3,
+  unarmored_movement: 10
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 4,
+  prof_bonus: 2,
+  martial_arts: 4,
+  ki_points: 4,
+  unarmored_movement: 10
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 5,
+  prof_bonus: 3,
+  martial_arts: 6,
+  ki_points: 5,
+  unarmored_movement: 10
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 6,
+  prof_bonus: 3,
+  martial_arts: 6,
+  ki_points: 6,
+  unarmored_movement: 15
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 7,
+  prof_bonus: 3,
+  martial_arts: 6,
+  ki_points: 7,
+  unarmored_movement: 15
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 8,
+  prof_bonus: 3,
+  martial_arts: 6,
+  ki_points: 8,
+  unarmored_movement: 15
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 9,
+  prof_bonus: 4,
+  martial_arts: 6,
+  ki_points: 9,
+  unarmored_movement: 15
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 10,
+  prof_bonus: 4,
+  martial_arts: 6,
+  ki_points: 10,
+  unarmored_movement: 20
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 11,
+  prof_bonus: 4,
+  martial_arts: 8,
+  ki_points: 11,
+  unarmored_movement: 20
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 12,
+  prof_bonus: 4,
+  martial_arts: 8,
+  ki_points: 12,
+  unarmored_movement: 20
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 13,
+  prof_bonus: 5,
+  martial_arts: 8,
+  ki_points: 13,
+  unarmored_movement: 20
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 14,
+  prof_bonus: 5,
+  martial_arts: 8,
+  ki_points: 14,
+  unarmored_movement: 25
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 15,
+  prof_bonus: 5,
+  martial_arts: 8,
+  ki_points: 15,
+  unarmored_movement: 25
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 16,
+  prof_bonus: 5,
+  martial_arts: 8,
+  ki_points: 16,
+  unarmored_movement: 25
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 17,
+  prof_bonus: 6,
+  martial_arts: 10,
+  ki_points: 17,
+  unarmored_movement: 25
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 18,
+  prof_bonus: 6,
+  martial_arts: 10,
+  ki_points: 18,
+  unarmored_movement: 30
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 19,
+  prof_bonus: 6,
+  martial_arts: 10,
+  ki_points: 19,
+  unarmored_movement: 30
+)
+
+Monk.create(
+  tradition: 'Way of the Open Hand',
+  level: 20,
+  prof_bonus: 6,
+  martial_arts: 10,
+  ki_points: 20,
+  unarmored_movement: 30
+)
+
+# == WAY OF THE FOUR ELEMENTS =============
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 3,
+  prof_bonus: 2,
+  martial_arts: 4,
+  ki_points: 3,
+  unarmored_movement: 10
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 4,
+  prof_bonus: 2,
+  martial_arts: 4,
+  ki_points: 4,
+  unarmored_movement: 10
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 5,
+  prof_bonus: 3,
+  martial_arts: 6,
+  ki_points: 5,
+  unarmored_movement: 10
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 6,
+  prof_bonus: 3,
+  martial_arts: 6,
+  ki_points: 6,
+  unarmored_movement: 15
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 7,
+  prof_bonus: 3,
+  martial_arts: 6,
+  ki_points: 7,
+  unarmored_movement: 15
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 8,
+  prof_bonus: 3,
+  martial_arts: 6,
+  ki_points: 8,
+  unarmored_movement: 15
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 9,
+  prof_bonus: 4,
+  martial_arts: 6,
+  ki_points: 9,
+  unarmored_movement: 15
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 10,
+  prof_bonus: 4,
+  martial_arts: 6,
+  ki_points: 10,
+  unarmored_movement: 20
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 11,
+  prof_bonus: 4,
+  martial_arts: 8,
+  ki_points: 11,
+  unarmored_movement: 20
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 12,
+  prof_bonus: 4,
+  martial_arts: 8,
+  ki_points: 12,
+  unarmored_movement: 20
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 13,
+  prof_bonus: 5,
+  martial_arts: 8,
+  ki_points: 13,
+  unarmored_movement: 20
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 14,
+  prof_bonus: 5,
+  martial_arts: 8,
+  ki_points: 14,
+  unarmored_movement: 25
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 15,
+  prof_bonus: 5,
+  martial_arts: 8,
+  ki_points: 15,
+  unarmored_movement: 25
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 16,
+  prof_bonus: 5,
+  martial_arts: 8,
+  ki_points: 16,
+  unarmored_movement: 25
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 17,
+  prof_bonus: 6,
+  martial_arts: 10,
+  ki_points: 17,
+  unarmored_movement: 25
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 18,
+  prof_bonus: 6,
+  martial_arts: 10,
+  ki_points: 18,
+  unarmored_movement: 30
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 19,
+  prof_bonus: 6,
+  martial_arts: 10,
+  ki_points: 19,
+  unarmored_movement: 30
+)
+
+Monk.create(
+  tradition: 'Way of the Four Elements',
+  level: 20,
+  prof_bonus: 6,
+  martial_arts: 10,
+  ki_points: 20,
+  unarmored_movement: 30
+)
