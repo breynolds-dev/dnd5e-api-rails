495bf06ae50a975b8a277785d9d9730e18c53cbe
arturusfury <bri.reynolds@gmail.com>
2016-12-07 11:14:32 -0600

    added basic knowledge domain entries

--- db/seeds/class/cleric.rb
+++ db/seeds/class/cleric.rb
@@ -31,6 +31,295 @@ Cleric.create(
 )
 
 # == KNOWLEDGE DOMAIN =====================
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 3,
+  prof_bonus: 2,
+  cantrips_known: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0,
+  spell_slots_level_06: 0,
+  spell_slots_level_07: 0,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 4,
+  prof_bonus: 2,
+  cantrips_known: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0,
+  spell_slots_level_06: 0,
+  spell_slots_level_07: 0,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 5,
+  prof_bonus: 3,
+  cantrips_known: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0,
+  spell_slots_level_06: 0,
+  spell_slots_level_07: 0,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 6,
+  prof_bonus: 3,
+  cantrips_known: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0,
+  spell_slots_level_06: 0,
+  spell_slots_level_07: 0,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 7,
+  prof_bonus: 3,
+  cantrips_known: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0,
+  spell_slots_level_06: 0,
+  spell_slots_level_07: 0,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 8,
+  prof_bonus: 3,
+  cantrips_known: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0,
+  spell_slots_level_06: 0,
+  spell_slots_level_07: 0,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 9,
+  prof_bonus: 4,
+  cantrips_known: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1,
+  spell_slots_level_06: 0,
+  spell_slots_level_07: 0,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 10,
+  prof_bonus: 4,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2,
+  spell_slots_level_06: 0,
+  spell_slots_level_07: 0,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 11,
+  prof_bonus: 4,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2,
+  spell_slots_level_06: 1,
+  spell_slots_level_07: 0,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 12,
+  prof_bonus: 4,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2,
+  spell_slots_level_06: 1,
+  spell_slots_level_07: 0,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 13,
+  prof_bonus: 5,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2,
+  spell_slots_level_06: 1,
+  spell_slots_level_07: 1,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 14,
+  prof_bonus: 5,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2,
+  spell_slots_level_06: 1,
+  spell_slots_level_07: 1,
+  spell_slots_level_08: 0,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 15,
+  prof_bonus: 5,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2,
+  spell_slots_level_06: 1,
+  spell_slots_level_07: 1,
+  spell_slots_level_08: 1,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 16,
+  prof_bonus: 5,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2,
+  spell_slots_level_06: 1,
+  spell_slots_level_07: 1,
+  spell_slots_level_08: 1,
+  spell_slots_level_09: 0
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 17,
+  prof_bonus: 6,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2,
+  spell_slots_level_06: 1,
+  spell_slots_level_07: 1,
+  spell_slots_level_08: 1,
+  spell_slots_level_09: 1
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 18,
+  prof_bonus: 6,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 3,
+  spell_slots_level_06: 1,
+  spell_slots_level_07: 1,
+  spell_slots_level_08: 1,
+  spell_slots_level_09: 1
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 19,
+  prof_bonus: 6,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 3,
+  spell_slots_level_06: 2,
+  spell_slots_level_07: 1,
+  spell_slots_level_08: 1,
+  spell_slots_level_09: 1
+)
+
+Cleric.create(
+  domain: 'Knowledge',
+  level: 20,
+  prof_bonus: 6,
+  cantrips_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 3,
+  spell_slots_level_06: 2,
+  spell_slots_level_07: 2,
+  spell_slots_level_08: 1,
+  spell_slots_level_09: 1
+)
+
 # == LIFE DOMAIN ==========================
 # == LIGHT DOMAIN =========================
 # == NATURE DOMAIN ========================
