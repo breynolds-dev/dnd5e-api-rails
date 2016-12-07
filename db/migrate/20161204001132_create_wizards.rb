cc30788e2caf5acdaee6afaa78b6c66dacacfa98
arturusfury <bri.reynolds@gmail.com>
2016-12-07 10:57:23 -0600

    create wizard table

--- db/migrate/20161204001132_create_wizards.rb
+++ db/migrate/20161204001132_create_wizards.rb
@@ -1,8 +1,20 @@
 class CreateWizards < ActiveRecord::Migration[5.0]
   def change
     create_table :wizards do |t|
-
-      t.timestamps
+      t.string :school
+      t.integer :level
+      t.integer :prof_bonus
+      t.integer :cantrips_known
+      t.integer :spells_known
+      t.integer :spell_slots_level_01
+      t.integer :spell_slots_level_02
+      t.integer :spell_slots_level_03
+      t.integer :spell_slots_level_04
+      t.integer :spell_slots_level_05
+      t.integer :spell_slots_level_06
+      t.integer :spell_slots_level_07
+      t.integer :spell_slots_level_08
+      t.integer :spell_slots_level_09
     end
   end
 end
