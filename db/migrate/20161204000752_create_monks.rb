3dc317bd63981b41f11054fd8c8470e0899d58a1
arturusfury <bri.reynolds@gmail.com>
2016-12-07 10:41:47 -0600

    added in the basic monk table

--- db/migrate/20161204000752_create_monks.rb
+++ db/migrate/20161204000752_create_monks.rb
@@ -1,8 +1,12 @@
 class CreateMonks < ActiveRecord::Migration[5.0]
   def change
     create_table :monks do |t|
-
-      t.timestamps
+      t.string :tradition
+      t.integer :level
+      t.integer :prof_bonus
+      t.integer :martial_arts
+      t.integer :ki_points
+      t.integer :unarmored_movement
     end
   end
 end
