844f4d935df88155bb42507bfc3f131bdb8ff4d1
arturusfury <bri.reynolds@gmail.com>
2016-12-07 12:17:32 -0600

    added our paladin seed data, closes #16

---
+++ db/seeds/class/paladins.rb
@@ -0,0 +1,621 @@
+Paladin.create(
+  oath: 'Paladin',
+  level: 1,
+  prof_bonus: 2,
+  spell_slots_level_01: 0,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Paladin',
+  level: 2,
+  prof_bonus: 2,
+  spell_slots_level_01: 2,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+# == OATH OF DEVOTION =====================
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 3,
+  prof_bonus: 2,
+  spell_slots_level_01: 3,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 4,
+  prof_bonus: 2,
+  spell_slots_level_01: 3,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 5,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 6,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 7,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 8,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 9,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 10,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 11,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 12,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 13,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 14,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 15,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 16,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 17,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 18,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 19,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2
+)
+
+Paladin.create(
+  oath: 'Oath of Devotion',
+  level: 20,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2
+)
+
+# == OATH OF THE ANCIENTS =================
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 3,
+  prof_bonus: 2,
+  spell_slots_level_01: 3,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 4,
+  prof_bonus: 2,
+  spell_slots_level_01: 3,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 5,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 6,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 7,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 8,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 9,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 10,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 11,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 12,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 13,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 14,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 15,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 16,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 17,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 18,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 19,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2
+)
+
+Paladin.create(
+  oath: 'Oath of the Ancients',
+  level: 20,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2
+)
+
+# == OATH OF VENGENCE =====================
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 3,
+  prof_bonus: 2,
+  spell_slots_level_01: 3,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 4,
+  prof_bonus: 2,
+  spell_slots_level_01: 3,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 5,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 6,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 7,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 8,
+  prof_bonus: 3,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 9,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 10,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 11,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 12,
+  prof_bonus: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 13,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 14,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 15,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 16,
+  prof_bonus: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 17,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 18,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 19,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2
+)
+
+Paladin.create(
+  oath: 'Oath of Vengence',
+  level: 20,
+  prof_bonus: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2
+)
