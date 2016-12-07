9543c1daf9e2c7de4a4ef0c2f050dd1225d7c717
arturusfury <bri.reynolds@gmail.com>
2016-12-07 10:38:02 -0600

    added our fighter table layout

--- db/migrate/20161204000701_create_fighters.rb
+++ db/migrate/20161204000701_create_fighters.rb
@@ -1,8 +1,9 @@
 class CreateFighters < ActiveRecord::Migration[5.0]
   def change
     create_table :fighters do |t|
-
-      t.timestamps
+      t.string :archetype
+      t.integer :level
+      t.integer :prof_bonus
     end
   end
 end
