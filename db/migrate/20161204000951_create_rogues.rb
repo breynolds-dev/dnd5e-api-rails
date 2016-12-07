0ac3efebf419e9bf774a324bab35e2c3bf7dfd7e
arturusfury <bri.reynolds@gmail.com>
2016-12-07 10:53:00 -0600

    added rouge class table

--- db/migrate/20161204000951_create_rogues.rb
+++ db/migrate/20161204000951_create_rogues.rb
@@ -1,8 +1,10 @@
 class CreateRogues < ActiveRecord::Migration[5.0]
   def change
     create_table :rogues do |t|
-
-      t.timestamps
+      t.string :archetype
+      t.integer :level
+      t.integer :prof_bonus
+      t.integer :sneak_attack
     end
   end
 end
