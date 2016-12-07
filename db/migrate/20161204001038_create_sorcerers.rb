183ab89670d6eb60a6576e83c9fb7a36069a88f0
arturusfury <bri.reynolds@gmail.com>
2016-12-07 10:53:58 -0600

    added the sorcerer class table

--- db/migrate/20161204001038_create_sorcerers.rb
+++ db/migrate/20161204001038_create_sorcerers.rb
@@ -1,8 +1,20 @@
 class CreateSorcerers < ActiveRecord::Migration[5.0]
   def change
     create_table :sorcerers do |t|
-
-      t.timestamps
+      t.string :metamagic
+      t.integer :level
+      t.integer :prof_bonus
+      t.integer :cantrips_known
+      t.integer :spells_known
+      t.integer :spell_slots_level_01
+      t.integer :spell_slots_level_02
+      t.integer :spell_slots_level_03
+      t.integer :spell_slots_level_04
+      t.integer :spell_slots_level_05
+      t.integer :spell_slots_level_06
+      t.integer :spell_slots_level_07
+      t.integer :spell_slots_level_08
+      t.integer :spell_slots_level_09
     end
   end
 end
