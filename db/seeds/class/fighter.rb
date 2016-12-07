5c384a887a2abd1a0759ff3f194d1995a513c0d5
arturusfury <bri.reynolds@gmail.com>
2016-12-07 12:09:36 -0600

    added basic fighter seed data, similar to the rogue though we need to work on the eldritch knight

---
+++ db/seeds/class/fighter.rb
@@ -0,0 +1,341 @@
+Fighter.create(
+  archetype: 'Fighter',
+  level: 1,
+  prof_bonus: 2
+)
+
+Fighter.create(
+  archetype: 'Fighter',
+  level: 2,
+  prof_bonus: 2
+)
+
+# == CHAMPION =============================
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 3,
+  prof_bonus: 2
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 4,
+  prof_bonus: 2
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 5,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 6,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 7,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 8,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 9,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 10,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 11,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 12,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 13,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 14,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 15,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 16,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 17,
+  prof_bonus: 6
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 18,
+  prof_bonus: 6
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 19,
+  prof_bonus: 6
+)
+
+Fighter.create(
+  archetype: 'Champion',
+  level: 20,
+  prof_bonus: 6
+)
+
+# == BATTLE MASTER ========================
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 3,
+  prof_bonus: 2
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 4,
+  prof_bonus: 2
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 5,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 6,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 7,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 8,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 9,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 10,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 11,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 12,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 13,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 14,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 15,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 16,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 17,
+  prof_bonus: 6
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 18,
+  prof_bonus: 6
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 19,
+  prof_bonus: 6
+)
+
+Fighter.create(
+  archetype: 'Battle Master',
+  level: 20,
+  prof_bonus: 6
+)
+
+# == ELDRITCH KNIGHT ======================
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 3,
+  prof_bonus: 2
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 4,
+  prof_bonus: 2
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 5,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 6,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 7,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 8,
+  prof_bonus: 3
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 9,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 10,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 11,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 12,
+  prof_bonus: 4
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 13,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 14,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 15,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 16,
+  prof_bonus: 5
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 17,
+  prof_bonus: 6
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 18,
+  prof_bonus: 6
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 19,
+  prof_bonus: 6
+)
+
+Fighter.create(
+  archetype: 'Eldritch Knight',
+  level: 20,
+  prof_bonus: 6
+)
