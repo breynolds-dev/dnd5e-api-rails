98df453ff5f22b1b214e14bb35d71747e9392b53
arturusfury <bri.reynolds@gmail.com>
2016-12-10 09:08:19 -0600

    added some logic to handle level 1 and 2 of a subclass

--- app/models/barbarian.rb
+++ app/models/barbarian.rb
@@ -1,15 +1,17 @@
 class Barbarian < ApplicationRecord
   def self.load_resource(subclass, level)
     if subclass && level
-      Barbarian.find_by(path: subclass, level: level)
+      if level.to_i > 2
+        Barbarian.find_by(path: subclass, level: level)
+      else
+        Barbarian.find_by(level: level)
+      end
     elsif !number?(subclass) && level.nil?
       Barbarian.where(path: subclass)
     else
       Barbarian.find(subclass)
     end
   end
-
-  private
 
   def self.number?(string)
     true if Float(string) rescue false
