cf5cdff51e34458355e0d395b8bcd882f1c19810
arturusfury <bri.reynolds@gmail.com>
2016-12-10 09:14:46 -0600

    removed some extra spacing and comments that didn't need to be there

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       # api/v1/characters/human/barbarian/berserker/16
