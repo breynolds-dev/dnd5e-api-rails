9ad9f787e6a57f7a5273a67a09e53341a6482c7c
arturusfury <bri.reynolds@gmail.com>
2016-12-12 16:44:54 -0600

    added basic factories to test our race models

---
+++ spec/factories/languages.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :draconic, class: :language do
+    name 'Draconic'
+    script 'Draconic'
+  end
+end
---
+++ spec/factories/races.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory :dragonborn, class: :race do
+    name 'Dragonborn'
+    desc 'Born of dragons as their name proclaims'
+    speed 30
+    ability_bonuses '2,0,0,0,0,1'
+    min_age 15
+    max_age 80
+    age_description 'Young dragonborn grow quickly.'
+    size 'medium'
+    size_description 'Your size is medium'
+    min_height 66
+    max_height 80
+    min_weight 175
+    max_weight 300
+    extra_skill_proficiencies 0
+    weapon_proficiencies ''
+    armor_proficiencies 'Light Armor,Medium Armor,Heavy Armor,Shields'
+  end
+end
