822ed4da946e730b21e223deb23eb690359c3ee4
arturusfury <bri.reynolds@gmail.com>
2016-12-12 10:02:21 -0600

    added basic seed data for our ranger class closes #12

---
+++ db/seeds/classes/rangers.rb
@@ -0,0 +1,459 @@
+Ranger.create(
+  archetype: 'Ranger',
+  level: 1,
+  prof_bonus: 2,
+  spells_known: 0,
+  spell_slots_level_01: 0,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Ranger',
+  level: 2,
+  prof_bonus: 2,
+  spells_known: 2,
+  spell_slots_level_01: 2,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+# == CHAMPION =============================
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 3,
+  prof_bonus: 2,
+  spells_known: 3,
+  spell_slots_level_01: 3,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 4,
+  prof_bonus: 2,
+  spells_known: 3,
+  spell_slots_level_01: 3,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 5,
+  prof_bonus: 3,
+  spells_known: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 6,
+  prof_bonus: 3,
+  spells_known: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 7,
+  prof_bonus: 3,
+  spells_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 8,
+  prof_bonus: 3,
+  spells_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 9,
+  prof_bonus: 4,
+  spells_known: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 10,
+  prof_bonus: 4,
+  spells_known: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 11,
+  prof_bonus: 4,
+  spells_known: 7,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 12,
+  prof_bonus: 4,
+  spells_known: 7,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 13,
+  prof_bonus: 5,
+  spells_known: 8,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 14,
+  prof_bonus: 5,
+  spells_known: 8,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 15,
+  prof_bonus: 5,
+  spells_known: 9,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 16,
+  prof_bonus: 5,
+  spells_known: 9,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 17,
+  prof_bonus: 6,
+  spells_known: 10,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 18,
+  prof_bonus: 6,
+  spells_known: 10,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 19,
+  prof_bonus: 6,
+  spells_known: 11,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2
+)
+
+Ranger.create(
+  archetype: 'Hunter',
+  level: 20,
+  prof_bonus: 6,
+  spells_known: 11,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2
+)
+
+# == BATTLE MASTER ========================
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 3,
+  prof_bonus: 2,
+  spells_known: 3,
+  spell_slots_level_01: 3,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 4,
+  prof_bonus: 2,
+  spells_known: 3,
+  spell_slots_level_01: 3,
+  spell_slots_level_02: 0,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 5,
+  prof_bonus: 3,
+  spells_known: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 6,
+  prof_bonus: 3,
+  spells_known: 4,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 2,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 7,
+  prof_bonus: 3,
+  spells_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 8,
+  prof_bonus: 3,
+  spells_known: 5,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 0,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 9,
+  prof_bonus: 4,
+  spells_known: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 10,
+  prof_bonus: 4,
+  spells_known: 6,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 2,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 11,
+  prof_bonus: 4,
+  spells_known: 7,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 12,
+  prof_bonus: 4,
+  spells_known: 7,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 0,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 13,
+  prof_bonus: 5,
+  spells_known: 8,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 14,
+  prof_bonus: 5,
+  spells_known: 8,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 1,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 15,
+  prof_bonus: 5,
+  spells_known: 9,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 16,
+  prof_bonus: 5,
+  spells_known: 9,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 2,
+  spell_slots_level_05: 0
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 17,
+  prof_bonus: 6,
+  spells_known: 10,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 18,
+  prof_bonus: 6,
+  spells_known: 10,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 1
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 19,
+  prof_bonus: 6,
+  spells_known: 11,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2
+)
+
+Ranger.create(
+  archetype: 'Beast Master',
+  level: 20,
+  prof_bonus: 6,
+  spells_known: 11,
+  spell_slots_level_01: 4,
+  spell_slots_level_02: 3,
+  spell_slots_level_03: 3,
+  spell_slots_level_04: 3,
+  spell_slots_level_05: 2
+)
