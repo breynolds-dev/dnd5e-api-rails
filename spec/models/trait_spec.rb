7bcbd0898b3dc926cece82a6ec51ef26642b33c6
arturusfury <bri.reynolds@gmail.com>
2016-12-13 08:32:41 -0600

    added basic trait testing for our model

--- spec/models/trait_spec.rb
+++ spec/models/trait_spec.rb
@@ -1,5 +1,9 @@
-require 'rails_helper'
-
 RSpec.describe Trait, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:draconic_ancestry) { FactoryGirl.create :draconic_ancestry }
+
+  it 'should allow the object to be created' do
+    expect(draconic_ancestry).to be_present
+  end
+
+  it { should have_one(:race) }
 end
