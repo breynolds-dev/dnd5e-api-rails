71283d1ccab9b61ff22dab32ea0208812101ddce
arturusfury <bri.reynolds@gmail.com>
2016-12-13 08:38:35 -0600

    added basic language testing

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -1,5 +1,9 @@
-require 'rails_helper'
-
 RSpec.describe Language, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:draconic) { FactoryGirl.create :draconic }
+
+  it 'should allow the object to be created' do
+    expect(draconic).to be_present
+  end
+
+  it { should have_many(:races) }
 end
