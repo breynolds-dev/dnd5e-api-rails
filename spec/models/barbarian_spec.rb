8388e1d93d2769478fc5bc8cdff02309f0d225a0
arturusfury <bri.reynolds@gmail.com>
2016-12-16 16:07:20 -0600

    added the extra specs from our bard to the barbarian and cleric tests

--- spec/models/barbarian_spec.rb
+++ spec/models/barbarian_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Barbarian, type: :model do
     expect(level_01).to be_present
     expect(level_02).to be_present
   end
+
+  it 'should not have a subclass (path) at level 1 & 2' do
+    expect(level_01.path).to eq('Barbarian')
+    expect(level_02.path).to eq('Barbarian')
+  end
+
+  it 'should have the correct level associated to it' do
+    expect(level_01.level).to eq(1)
+    expect(level_02.level).to eq(2)
+  end
 end
--- spec/models/cleric_spec.rb
+++ spec/models/cleric_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Cleric, type: :model do
     expect(level_01).to be_present
     expect(level_02).to be_present
   end
+
+  it 'should not have a subclass (domain) at level 1 & 2' do
+    expect(level_01.domain).to eq('Cleric')
+    expect(level_02.domain).to eq('Cleric')
+  end
+
+  it 'should have the correct level associated to it' do
+    expect(level_01.level).to eq(1)
+    expect(level_02.level).to eq(2)
+  end
 end
