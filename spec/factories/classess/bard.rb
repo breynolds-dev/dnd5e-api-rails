e22c8ccb1fd3de0171cb91b3e80bdaf4fb614bbf
arturusfury <bri.reynolds@gmail.com>
2016-12-16 15:49:15 -0600

    reworked our factories for the bard and cleric classes

--- spec/factories/classess/bard.rb
+++ spec/factories/classess/bard.rb
@@ -1,17 +1,35 @@
 FactoryGirl.define do
   factory :bard_level_01, class: :bard do
-    path 'Barbarian'
+    college 'Bard'
     level 1
     prof_bonus 2
-    rage_count 1
-    rage_damage_bonus 2
+    cantrips_known 4
+    spells_known 2
+    spell_slots_level_01 2
+    spell_slots_level_02 0
+    spell_slots_level_03 0
+    spell_slots_level_04 0
+    spell_slots_level_05 0
+    spell_slots_level_06 0
+    spell_slots_level_07 0
+    spell_slots_level_08 0
+    spell_slots_level_09 0
   end
 
   factory :bard_level_02, class: :bard do
-    path 'Barbarian'
+    college 'Bard'
     level 2
     prof_bonus 2
-    rage_count 2
-    rage_damage_bonus 2
+    cantrips_known 4
+    spells_known 2
+    spell_slots_level_01 2
+    spell_slots_level_02 0
+    spell_slots_level_03 0
+    spell_slots_level_04 0
+    spell_slots_level_05 0
+    spell_slots_level_06 0
+    spell_slots_level_07 0
+    spell_slots_level_08 0
+    spell_slots_level_09 0
   end
 end
--- spec/factories/classess/cleric.rb
+++ spec/factories/classess/cleric.rb
@@ -1,17 +1,35 @@
 FactoryGirl.define do
   factory :cleric_level_01, class: :cleric do
-    path 'Barbarian'
+    college 'Cleric'
     level 1
     prof_bonus 2
-    rage_count 1
-    rage_damage_bonus 2
+    cantrips_known 4
+    spells_known 2
+    spell_slots_level_01 2
+    spell_slots_level_02 0
+    spell_slots_level_03 0
+    spell_slots_level_04 0
+    spell_slots_level_05 0
+    spell_slots_level_06 0
+    spell_slots_level_07 0
+    spell_slots_level_08 0
+    spell_slots_level_09 0
   end
 
   factory :cleric_level_02, class: :cleric do
-    path 'Barbarian'
+    college 'Cleric'
     level 2
     prof_bonus 2
-    rage_count 2
-    rage_damage_bonus 2
+    cantrips_known 4
+    spells_known 2
+    spell_slots_level_01 2
+    spell_slots_level_02 0
+    spell_slots_level_03 0
+    spell_slots_level_04 0
+    spell_slots_level_05 0
+    spell_slots_level_06 0
+    spell_slots_level_07 0
+    spell_slots_level_08 0
+    spell_slots_level_09 0
   end
 end
