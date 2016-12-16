737c609e1ad73bfc8ae6805026e8afd6bbf1bea9
arturusfury <bri.reynolds@gmail.com>
2016-12-16 15:57:08 -0600

    finished the last of the class additions

--- spec/factories/classess/warlock.rb
+++ spec/factories/classess/warlock.rb
@@ -1,17 +1,23 @@
 FactoryGirl.define do
   factory :warlock_level_01, class: :warlock do
-    path 'Barbarian'
+    pact 'Warlock'
     level 1
     prof_bonus 2
-    rage_count 1
-    rage_damage_bonus 2
+    cantrips_known 2
+    spells_known 2
+    spell_slots 1
+    slot_level 1
+    invocations_known 0
   end
 
   factory :warlock_level_02, class: :warlock do
-    path 'Barbarian'
+    pact 'Warlock'
     level 2
     prof_bonus 2
-    rage_count 2
-    rage_damage_bonus 2
+    cantrips_known 2
+    spells_known 2
+    spell_slots 1
+    slot_level 1
+    invocations_known 0
   end
 end
--- spec/factories/classess/wizard.rb
+++ spec/factories/classess/wizard.rb
@@ -1,17 +1,33 @@
 FactoryGirl.define do
   factory :wizard_level_01, class: :wizard do
-    path 'Barbarian'
+    school 'Wizard'
     level 1
     prof_bonus 2
-    rage_count 1
-    rage_damage_bonus 2
+    cantrips_known 2
+    spell_slots_level_01 2
+    spell_slots_level_02 0
+    spell_slots_level_03 0
+    spell_slots_level_04 0
+    spell_slots_level_05 0
+    spell_slots_level_06 0
+    spell_slots_level_07 0
+    spell_slots_level_08 0
+    spell_slots_level_09 0
   end
 
   factory :wizard_level_02, class: :wizard do
-    path 'Barbarian'
+    school 'Wizard'
     level 2
     prof_bonus 2
-    rage_count 2
-    rage_damage_bonus 2
+    cantrips_known 2
+    spell_slots_level_01 2
+    spell_slots_level_02 0
+    spell_slots_level_03 0
+    spell_slots_level_04 0
+    spell_slots_level_05 0
+    spell_slots_level_06 0
+    spell_slots_level_07 0
+    spell_slots_level_08 0
+    spell_slots_level_09 0
   end
 end
