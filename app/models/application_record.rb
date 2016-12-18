5d4412cb22bb9963d159e6a5123cb316d5abe1d2
arturusfury <bri.reynolds@gmail.com>
2016-12-18 17:13:56 -0600

    added a make readable method to help our friendly routes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.make_readable(string)
+    deslug(string).to_s
+  end
+
+  private
+
+  def self.deslug(string)
+    string.downcase.tr('-', ' ')
+  end
 end
