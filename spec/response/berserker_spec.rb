2977f931f3ff6ed8fcd25fbc4c04ce2b796457b8
arturusfury <bri.reynolds@gmail.com>
2016-12-19 14:13:16 -0600

    changed our rspec testing to allow the use of let so we can preload and cache our testing data

--- spec/response/berserker_spec.rb
+++ spec/response/berserker_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Berserker', type: :request do
-  def parsed_response
-    JSON.parse(response.body)
-  end
-
-  def create_database
+  let(:load_barbarians) do
     FactoryGirl.create :barbarian_level_01
     FactoryGirl.create :barbarian_level_02
     FactoryGirl.create :barbarian_level_19_berserker
     FactoryGirl.create :barbarian_level_20_berserker
     FactoryGirl.create :barbarian_level_20_totem
   end
+
+  let(:parsed_response) { JSON.parse(response.body) }
 
   describe 'GET /v1/classes/barbarians' do
     it 'returns 200 response' do
@@ -22,7 +20,7 @@ RSpec.describe 'Berserker', type: :request do
     end
 
     it 'returns an array of races' do
-      create_database
+      load_barbarians
       get '/v1/classes/barbarians'
 
       expect(response.status).to eq(200)
@@ -33,12 +31,7 @@ RSpec.describe 'Berserker', type: :request do
 
   describe 'GET /v1/classes/barbarians/(subclass)' do
     it 'returns an array of bersker subclass levels' do
-      FactoryGirl.create :barbarian_level_01
-      FactoryGirl.create :barbarian_level_02
-      FactoryGirl.create :barbarian_level_19_berserker
-      FactoryGirl.create :barbarian_level_20_berserker
-      FactoryGirl.create :barbarian_level_20_totem
-
+      load_barbarians
       get '/v1/classes/barbarians/Berserker'
 
       expect(response.status).to eq(200)
@@ -49,12 +42,7 @@ RSpec.describe 'Berserker', type: :request do
 
   describe 'GET /v1/classes/barbarians/(subclass)/(level)' do
     it 'returns the correct entry based on subclass and model' do
-      FactoryGirl.create :barbarian_level_01
-      FactoryGirl.create :barbarian_level_02
-      FactoryGirl.create :barbarian_level_19_berserker
-      FactoryGirl.create :barbarian_level_20_berserker
-      FactoryGirl.create :barbarian_level_20_totem
-
+      load_barbarians
       get '/v1/classes/barbarians/Berserker/20'
 
       expect(response.status).to eq(200)
@@ -65,12 +53,7 @@ RSpec.describe 'Berserker', type: :request do
 
   describe 'GET /v1/classes/barbarians/(level)' do
     it 'returns an array of level specefic class entries' do
-      FactoryGirl.create :barbarian_level_01
-      FactoryGirl.create :barbarian_level_02
-      FactoryGirl.create :barbarian_level_19_berserker
-      FactoryGirl.create :barbarian_level_20_berserker
-      FactoryGirl.create :barbarian_level_20_totem
-
+      load_barbarians
       get '/v1/classes/barbarians/20'
 
       expect(response.status).to eq(200)
