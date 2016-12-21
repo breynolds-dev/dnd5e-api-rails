f30f38b77b3a73bdcc6992dc51d5b8ab0d315750
arturusfury <bri.reynolds@gmail.com>
2016-12-20 21:35:27 -0600

    added a few more language factories

--- spec/factories/languages.rb
+++ spec/factories/languages.rb
@@ -1,6 +1,26 @@
 FactoryGirl.define do
+  factory :common, class: :language do
+    name 'Common'
+    script 'Common'
+  end
+
   factory :draconic, class: :language do
     name 'Draconic'
     script 'Draconic'
+  end
+
+  factory :dwarven, class: :language do
+    name 'Dwarven'
+    script 'Dwarven'
+  end
+
+  factory :goblin, class: :language do
+    name 'Goblin'
+    script 'Dwarven'
+  end
+
+  factory :infernal, class: :language do
+    name 'Infernal'
+    script 'Infernal'
   end
 end
