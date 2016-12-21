961d61d252698f1c3c2f72774202afc7fc9d5ea0
arturusfury <bri.reynolds@gmail.com>
2016-12-21 15:50:04 -0600

    added factories and monk tests to match

--- spec/factories/classess/monk.rb
+++ spec/factories/classess/monk.rb
@@ -13,7 +13,34 @@ FactoryGirl.define do
     level 2
     prof_bonus 2
     martial_arts 4
-    ki_points 0
-    unarmored_movement 0
+    ki_points 2
+    unarmored_movement 10
+  end
+
+  factory :monk_level_19_hand, class: Classes::Monk do
+    subclass 'Way of the Open Hand'
+    level 19
+    prof_bonus 6
+    martial_arts 10
+    ki_points 19
+    unarmored_movement 30
+  end
+
+  factory :monk_level_20_hand, class: Classes::Monk do
+    subclass 'Way of the Open Hand'
+    level 20
+    prof_bonus 6
+    martial_arts 10
+    ki_points 20
+    unarmored_movement 30
+  end
+
+  factory :monk_level_20_elements, class: Classes::Monk do
+    subclass 'Way of the Four Elements'
+    level 20
+    prof_bonus 6
+    martial_arts 10
+    ki_points 20
+    unarmored_movement 30
   end
 end
