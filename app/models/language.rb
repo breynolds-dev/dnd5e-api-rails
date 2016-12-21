8db6ec8c6a872d7ed37cd1182ff3acb9dce1e37d
arturusfury <bri.reynolds@gmail.com>
2016-12-20 20:54:57 -0600

    added a load_language method to be able to find our language no matter the case

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,8 @@
 class Language < ApplicationRecord
   has_many :joins_language
   has_many :races, through: :joins_language
+
+  def self.load_language(name)
+    find_by('lower(name) = ?', make_readable(name.downcase))
+  end
 end
