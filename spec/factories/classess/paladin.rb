d7d433635cc3689ff5e21402ec41629535821aa2
arturusfury <bri.reynolds@gmail.com>
2016-12-21 15:54:31 -0600

    completed our paladin factory and test cases

--- spec/factories/classess/paladin.rb
+++ spec/factories/classess/paladin.rb
@@ -20,4 +20,37 @@ FactoryGirl.define do
     spell_slots_level_04 0
     spell_slots_level_05 0
   end
+
+  factory :paladin_level_19_devotion, class: Classes::Paladin do
+    subclass 'Oath of Devotion'
+    level 19
+    prof_bonus 6
+    spell_slots_level_01 4
+    spell_slots_level_02 3
+    spell_slots_level_03 3
+    spell_slots_level_04 3
+    spell_slots_level_05 2
+  end
+
+  factory :paladin_level_20_devotion, class: Classes::Paladin do
+    subclass 'Oath of Devotion'
+    level 20
+    prof_bonus 6
+    spell_slots_level_01 4
+    spell_slots_level_02 3
+    spell_slots_level_03 3
+    spell_slots_level_04 3
+    spell_slots_level_05 2
+  end
+
+  factory :paladin_level_20_ancients, class: Classes::Paladin do
+    subclass 'Oath of the Ancients'
+    level 20
+    prof_bonus 6
+    spell_slots_level_01 4
+    spell_slots_level_02 3
+    spell_slots_level_03 3
+    spell_slots_level_04 3
+    spell_slots_level_05 2
+  end
 end
