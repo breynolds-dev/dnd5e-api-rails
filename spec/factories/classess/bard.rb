2a3761d9d89fb43d17a59bab3ce6d8ab001e3d35
arturusfury <bri.reynolds@gmail.com>
2016-12-21 15:15:42 -0600

    updated our factories for the new wizard and bard specs

--- spec/factories/classess/bard.rb
+++ spec/factories/classess/bard.rb
@@ -32,4 +32,55 @@ FactoryGirl.define do
     spell_slots_level_08 0
     spell_slots_level_09 0
   end
+
+  factory :bard_level_19_lore, class: Classes::Bard do
+    subclass 'College of Lore'
+    level 19
+    prof_bonus 2
+    cantrips_known 2
+    spells_known 22
+    spell_slots_level_01 4
+    spell_slots_level_02 3
+    spell_slots_level_03 3
+    spell_slots_level_04 3
+    spell_slots_level_05 3
+    spell_slots_level_06 2
+    spell_slots_level_07 1
+    spell_slots_level_08 1
+    spell_slots_level_09 1
+  end
+
+  factory :bard_level_20_lore, class: Classes::Bard do
+    subclass 'College of Lore'
+    level 20
+    prof_bonus 6
+    cantrips_known 5
+    spells_known 22
+    spell_slots_level_01 4
+    spell_slots_level_02 3
+    spell_slots_level_03 3
+    spell_slots_level_04 3
+    spell_slots_level_05 3
+    spell_slots_level_06 2
+    spell_slots_level_07 2
+    spell_slots_level_08 1
+    spell_slots_level_09 1
+  end
+
+  factory :bard_level_20_valor, class: Classes::Bard do
+    subclass 'College of Valor'
+    level 20
+    prof_bonus 6
+    cantrips_known 5
+    spells_known 22
+    spell_slots_level_01 4
+    spell_slots_level_02 3
+    spell_slots_level_03 3
+    spell_slots_level_04 3
+    spell_slots_level_05 3
+    spell_slots_level_06 2
+    spell_slots_level_07 2
+    spell_slots_level_08 1
+    spell_slots_level_09 1
+  end
 end
