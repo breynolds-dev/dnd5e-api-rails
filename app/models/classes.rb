82f3fee97d5ab4b6ccd41d4b1f3921164ff06831
arturusfury <bri.reynolds@gmail.com>
2016-12-21 11:10:03 -0600

    refactored and added a new method match_subclass_level

--- app/models/classes.rb
+++ app/models/classes.rb
@@ -3,16 +3,22 @@ class Classes < ApplicationRecord
   self.abstract_class = true
 
   def self.load_resource(subclass, level)
-    if subclass && level
-      if level.to_i > 2
-        find_by_subclass_level(subclass, level)
-      else
-        find_by_level(level).first
-      end
+    if number?(subclass)
+      find(subclass)
+    elsif subclass && level
+      match_subclass_level(subclass, level)
     elsif !number?(subclass) && level.nil?
       find_by_subclass(subclass)
     else
       find_by_level(subclass)
+    end
+  end
+
+  def self.match_subclass_level(subclass, level)
+    if level.to_i > 2
+      find_by_subclass_level(subclass, level)
+    else
+      find_by_level(level).first
     end
   end
 
@@ -29,6 +35,7 @@ class Classes < ApplicationRecord
   end
 
   def self.find_by_subclass_level(subclass, level)
-    find_by('lower(subclass) = ? AND level = ?', make_readable(subclass.downcase), level)
+    find_by('lower(subclass) = ? AND level = ?',
+            make_readable(subclass.downcase), level)
   end
 end
