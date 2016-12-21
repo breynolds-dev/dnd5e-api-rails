6e99d950b76636cc473511b67e4b1812e59a402e
arturusfury <bri.reynolds@gmail.com>
2016-12-21 16:20:56 -0600

    added tests and factories for our warlock class

--- spec/factories/classess/warlock.rb
+++ spec/factories/classess/warlock.rb
@@ -20,4 +20,37 @@ FactoryGirl.define do
     slot_level 1
     invocations_known 0
   end
+
+  factory :warlock_level_19_archfey, class: Classes::Warlock do
+    subclass 'Archfey'
+    level 19
+    prof_bonus 6
+    cantrips_known 4
+    spells_known 15
+    spell_slots 4
+    slot_level 5
+    invocations_known 8
+  end
+
+  factory :warlock_level_20_archfey, class: Classes::Warlock do
+    subclass 'Archfey'
+    level 20
+    prof_bonus 6
+    cantrips_known 4
+    spells_known 15
+    spell_slots 4
+    slot_level 5
+    invocations_known 8
+  end
+
+  factory :warlock_level_20_great, class: Classes::Warlock do
+    subclass 'Great One'
+    level 20
+    prof_bonus 6
+    cantrips_known 4
+    spells_known 15
+    spell_slots 4
+    slot_level 5
+    invocations_known 8
+  end
 end
