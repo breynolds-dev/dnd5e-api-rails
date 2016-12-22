90beccff48dafc609d7b63fef1d4aa09e31f9ceb
arturusfury <bri.reynolds@gmail.com>
2016-12-22 10:26:28 -0600

    added our load_trait method to handle friendly urls

--- app/models/trait.rb
+++ app/models/trait.rb
@@ -1,4 +1,8 @@
 class Trait < ApplicationRecord
   has_one :joins_trait
   has_one :race, through: :joins_trait
+
+  def self.load_trait(trait)
+    find_by('lower(race_name) = ?', make_readable(trait))
+  end
 end
