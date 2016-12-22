9b04b7bac38bea82bf5bf3811e4ff5c3f99ffb7e
arturusfury <bri.reynolds@gmail.com>
2016-12-22 15:56:42 -0600

    updated our serializer to match the new relationship and include the ability in the show action

--- app/serializers/skill_serializer.rb
+++ app/serializers/skill_serializer.rb
@@ -5,9 +5,17 @@ class SkillSerializer < RouteSerializer
     object.desc.split('\n\r')
   end
 
+  def ability
+    {
+      name: object.ability.name,
+      url: "#{root_url}/ability/#{make_params(object.ability.name)}"
+    }
+  end
+
   def links
     {
-      self: "#{root_url}/skills/#{make_params(object.name)}"
+      self: "#{root_url}/skills/#{make_params(object.name)}",
+      ability: "#{root_url}/ability/#{make_params(object.ability.name)}"
     }
   end
 end
