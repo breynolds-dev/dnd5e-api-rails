9fd58539a902e96a9942dd9f773183fd81236cdc
arturusfury <bri.reynolds@gmail.com>
2016-12-22 10:26:10 -0600

    added a conroller to handle the actions from our routes

---
+++ app/controllers/api/v1/traits_controller.rb
@@ -0,0 +1,17 @@
+class API::V1::TraitsController < ApplicationController
+  respond_to :json
+
+  def index
+    render json: Trait.all, each_serializer: Indexes::TraitIndexSerializer
+  end
+
+  def show
+    trait = Trait.load_trait(params[:trait])
+
+    if trait.nil?
+      resource_not_found('traits', params[:trait])
+    else
+      respond_with(trait)
+    end
+  end
+end
