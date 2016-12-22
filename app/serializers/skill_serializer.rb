3ab58f83842bf87faf3680f584ceadda51691cfa
arturusfury <bri.reynolds@gmail.com>
2016-12-21 20:55:24 -0600

    added some self referential links to the resource

--- app/serializers/skill_serializer.rb
+++ app/serializers/skill_serializer.rb
@@ -1,7 +1,13 @@
-class SkillSerializer < ActiveModel::Serializer
-  attributes :id, :name, :ability, :description
+class SkillSerializer < RouteSerializer
+  attributes :id, :name, :ability, :description, :links
 
   def description
     object.desc.split('\n\r')
+  end
+
+  def links
+    {
+      self: "#{root_url}/skill/#{make_params(object.name)}"
+    }
   end
 end
