2658ae91d9f386fe5ca4bcdbbff24f44023022ff
arturusfury <bri.reynolds@gmail.com>
2016-12-22 10:26:55 -0600

    added a serializer to handle our index action and then our show action, also included links to related objects

--- app/serializers/trait_serializer.rb
+++ app/serializers/trait_serializer.rb
@@ -1,13 +1,36 @@
-class TraitSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :range, :links
+class TraitSerializer < RouteSerializer
+  attributes :id, :name, :associated_race, :description, :range, :links
 
   def name
     object.race_name
   end
 
+  def associated_race
+    if object.race.subrace.nil?
+      {
+        name: object.race.name,
+        url: race_path
+      }
+    else
+      {
+        name: object.race.subrace,
+        url: race_path
+      }
+    end
+  end
+
   def links
     {
-      self: "#{root_url}/traits/#{make_params(object.race_name)}"
+      self: "#{root_url}/traits/#{make_params(object.race_name)}",
+      race: race_path
     }
+  end
+
+  def race_path
+    if object.race.subrace.nil?
+      "#{root_url}/races/#{make_params(object.race.name)}"
+    else
+      "#{root_url}/races/#{make_params(object.race.name)}/#{make_params(object.race.subrace)}"
+    end
   end
 end
