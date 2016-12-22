c55d588b0b96d303524d9b59b0c47f72c55cee9f
arturusfury <bri.reynolds@gmail.com>
2016-12-21 21:23:59 -0600

    added attributes to our trait serializer, just need some routes and a controller

--- app/serializers/trait_serializer.rb
+++ app/serializers/trait_serializer.rb
@@ -1,3 +1,13 @@
 class TraitSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :description, :range, :links
+
+  def name
+    object.race_name
+  end
+
+  def links
+    {
+      self: "#{root_url}/traits/#{make_params(object.race_name)}"
+    }
+  end
 end
