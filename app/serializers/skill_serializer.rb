65e21e793330a8d45680d4865ae2ecea7bcc93d8
arturusfury <bri.reynolds@gmail.com>
2016-12-23 15:18:35 -0600

    missed a file during last commit, updated the related links

--- app/serializers/skill_serializer.rb
+++ app/serializers/skill_serializer.rb
@@ -8,14 +8,20 @@ class SkillSerializer < RouteSerializer
   def ability
     {
       name: object.ability.name,
-      url: "#{root_url}/ability/#{make_params(object.ability.name)}"
+      url: "#{root_url}/abilities/#{make_params(object.ability.name)}"
     }
   end
 
   def links
     {
       self: "#{root_url}/skills/#{make_params(object.name)}",
-      ability: "#{root_url}/ability/#{make_params(object.ability.name)}"
+      related: related_links
     }
+  end
+
+  def related_links
+    related_links = {}
+    related_links.store(make_params(object.ability.name), "#{root_url}/abilities/#{make_params(object.ability.name)}")
+    related_links
   end
 end
