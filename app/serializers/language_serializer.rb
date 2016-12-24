c2724c9f0479d5b7ccd4992fb577617a95b580cd
Brian Reynolds <bri.reynolds@gmail.com>
2016-12-24 01:58:56 +0000

    initial commit for issue #60

--- app/serializers/language_serializer.rb
+++ app/serializers/language_serializer.rb
@@ -1,9 +1,15 @@
 class LanguageSerializer < RouteSerializer
-  attributes :id, :name, :script, :links
+  attributes :id, :name, :script, :language_speakers, :links
 
   def links
     {
-      self: "#{root_url}/languages/#{make_params(object.name)}"
+      self: "#{root_url}/languages/#{make_params(object.name)}",
+    }
+  end
+  
+  def language_speakers
+    {
+      known_speakers: "#{object.races}"
     }
   end
 end
