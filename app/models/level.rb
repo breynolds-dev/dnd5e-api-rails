79cf27a7eb4edc8f8d4e6786fcb7c002e9d212b8
arturusfury <bri.reynolds@gmail.com>
2016-12-24 17:13:07 -0600

    added a find_level method to be able to grab a classes level

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,3 +1,8 @@
 class Level < ApplicationRecord
   belongs_to :class_name
+
+  def self.find_level(subclass, level)
+    find_by('lower(subclass) = ? AND number = ?',
+            make_readable(subclass.downcase), level)
+  end
 end
