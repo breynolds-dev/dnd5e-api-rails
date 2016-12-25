5031fd5354933daa37fa2bce94205ae16faea808
arturusfury <bri.reynolds@gmail.com>
2016-12-24 21:50:41 -0600

    updated our class_names table to add more data that gets added to our reponses

--- db/migrate/20161203033954_create_class_names.rb
+++ db/migrate/20161203033954_create_class_names.rb
@@ -2,6 +2,12 @@ class CreateClassNames < ActiveRecord::Migration[5.0]
   def change
     create_table :class_names do |t|
       t.string :name, null: false
+      t.string :desc, default: ''
+      t.string :creating_a, default: ''
+      t.string :quick_build, default: ''
+      t.string :armor_proficiencies, default: ''
+      t.string :weapon_proficiencies, default: ''
+      t.string :tools, default: ''
     end
   end
 end
