df59d9240371ae8346f15ca4032cfa3819e08e18
arturusfury <bri.reynolds@gmail.com>
2016-12-25 00:49:18 -0600

    migrated our database after creating new models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222214319) do
+ActiveRecord::Schema.define(version: 20161225034322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,23 @@ ActiveRecord::Schema.define(version: 20161222214319) do
   end
 
   create_table "class_names", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name",                              null: false
+    t.string "desc",                 default: ""
+    t.string "creating_a",           default: ""
+    t.string "quick_build",          default: ""
+    t.string "armor_proficiencies",  default: ""
+    t.string "weapon_proficiencies", default: ""
+    t.string "tools",                default: ""
+  end
+
+  create_table "class_saving_throws", force: :cascade do |t|
+    t.integer "class_name_id", null: false
+    t.integer "ability_id",    null: false
+  end
+
+  create_table "class_skills", force: :cascade do |t|
+    t.integer "class_name_id", null: false
+    t.integer "skill_id",      null: false
   end
 
   create_table "feats", force: :cascade do |t|
