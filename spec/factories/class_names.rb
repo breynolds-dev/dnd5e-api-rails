3bcfff1bc75e97d8e2e7db432ed1b532baad1068
arturusfury <bri.reynolds@gmail.com>
2016-12-25 20:53:35 -0600

    updated our tests for the class_name model to test all the associated responses

--- spec/factories/class_names.rb
+++ spec/factories/class_names.rb
@@ -1,33 +1,47 @@
 FactoryGirl.define do
   factory :barbarian, class: :class_name do
     name 'Barbarian'
+    desc ''
+    creating_a ''
+    quick_build ''
+    hit_die 12
+    armor_proficiencies 'Light Armor,Medium Armor,Shields'
+    weapon_proficiencies 'Simple Weapons,Martial Weapons'
+    tools ''
   end
 
   factory :bard, class: :class_name do
     name 'Bard'
+    hit_die 8
   end
 
   factory :fighter, class: :class_name do
     name 'Fighter'
+    hit_die 10
   end
 
   factory :monk, class: :class_name do
     name 'Monk'
+    hit_die 8
   end
 
   factory :ranger, class: :class_name do
     name 'Ranger'
+    hit_die 10
   end
 
   factory :rogue, class: :class_name do
     name 'Rogue'
+    hit_die 8
   end
 
   factory :sorcerer, class: :class_name do
     name 'Sorcerer'
+    hit_die 6
   end
 
   factory :warlock, class: :class_name do
     name 'Warlock'
+    hit_die 8
   end
 end
--- spec/requests/class_name_spec.rb
+++ spec/requests/class_name_spec.rb
@@ -13,8 +13,6 @@ RSpec.describe 'Class Name', type: :request do
     FactoryGirl.create(:warlock)
   end
 
-
-
   describe 'GET /v1/classes/' do
     it 'returns an array of classes' do
       load_classes
@@ -34,22 +32,60 @@ RSpec.describe 'Class Name', type: :request do
 
       expect(response.status).to eq(200)
       expect(parsed_response['name']).to eq('Barbarian')
+      expect(parsed_response['url']).to eq('http://5e-api.com/v1/classes/barbarian')
     end
 
     it 'returns the subclass options for the class object' do
       class_name = FactoryGirl.create(:barbarian)
-      class_name.levels.create(
-        subclass: 'Berserker',
-        number: 20
+      class_name.levels.create(subclass: 'Barbarian', number: 1)
+      class_name.levels.create(subclass: 'Berserker', number: 20)
+      class_name.levels.create(subclass: 'Totem Warrior', number: 20)
+      get '/v1/classes/barbarian'
+      expect(response.status).to eq(200)
+      expect(parsed_response['subclasses'].collect {|subcls| subcls['name'] }).to eq(
+        ['Berserker', 'Totem Warrior']
       )
-      class_name.levels.create(
-        subclass: 'Totem Warrior',
-        number: 20
+    end
+
+    it 'returns an array of primary class ability choices' do
+      ClassPrimaryAbility.create(
+        class_name: FactoryGirl.create(:barbarian),
+        ability: FactoryGirl.create(:strength)
       )
       get '/v1/classes/barbarian'
       expect(response.status).to eq(200)
-      expect(parsed_response['subclasses']).to eq(
-        ['Berserker', 'Totem Warrior']
+      expect(parsed_response['primary_abilities'].collect { |abil| abil['name'] }).to eq(
+        ['Strength']
+      )
+    end
+
+    it 'returns an array of ability saving throws' do
+      barb = FactoryGirl.create(:barbarian)
+      ClassSavingThrow.create(
+        class_name: barb,
+        ability: FactoryGirl.create(:strength)
+      )
+      ClassSavingThrow.create(
+        class_name: barb,
+        ability: FactoryGirl.create(:dexterity)
+      )
+      get '/v1/classes/barbarian'
+      expect(response.status).to eq(200)
+      expect(parsed_response['saving_throws'].collect { |abil| abil['name'] }).to eq(
+        %w(Strength Dexterity)
+      )
+    end
+
+    it 'returns an array of armor and weapon proficiencies' do
+      FactoryGirl.create(:barbarian)
+      get '/v1/classes/barbarian'
+
+      expect(response.status).to eq(200)
+      expect(parsed_response['armor_proficiencies']).to eq(
+        ['Light Armor', 'Medium Armor', 'Shields']
+      )
+      expect(parsed_response['weapon_proficiencies']).to eq(
+        ['Simple Weapons', 'Martial Weapons']
       )
     end
   end
