b93c5a626b7ac7bfc94598c812c9abc03cbef88f
arturusfury <bri.reynolds@gmail.com>
2016-12-25 21:56:20 -0600

    removed the lookup by id for our ability models, lookups should be by name only

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,6 @@ class Ability < ApplicationRecord
   has_many :skills
 
   def self.load_ability(name)
-    if !number?(name)
-      find_by('lower(name) = ?', make_readable(name.downcase))
-    else
-      find(name)
-    end
+    find_by('lower(name) = ?', make_readable(name.downcase)) unless number?(name)
   end
 end
