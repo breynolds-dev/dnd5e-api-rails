dc53928848e577870fbb4205279048436d7f3749
arturusfury <bri.reynolds@gmail.com>
2016-12-25 21:26:02 -0600

    added back in testing of our subclass levels

--- spec/requests/levels_spec.rb
+++ spec/requests/levels_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'Levels', type: :request do
   let(:load_barbarians) do
     class_name = FactoryGirl.create(:barbarian)
     class_name.levels.create(subclass: 'Barbarian', number: 1)
+    class_name.levels.create(subclass: 'Barbarian', number: 2)
+    class_name.levels.create(subclass: 'Berserker', number: 19)
     class_name.levels.create(subclass: 'Berserker', number: 20)
     class_name.levels.create(subclass: 'Totem Warrior', number: 20)
   end
@@ -39,43 +41,43 @@ RSpec.describe 'Levels', type: :request do
       expect(parsed_response.length).to eq(2)
     end
   end
-  
-  # describe 'GET /v1/classes/barbarian/:subclass' do
-  #   it 'returns a 404 with an invalid subclass' do
-  #     load_barbarians
-  #     get '/v1/classes/barbarian/rager'
-  #     expect(response.status).to eq(404)
-  #     expect(parsed_response['path']).to eq('/v1/classes/barbarian/rager')
-  #   end
-  #
-  #   it 'returns an array of berserker subclass levels' do
-  #     load_barbarians
-  #     get '/v1/classes/barbarian/Berserker'
-  #
-  #     expect(response.status).to eq(200)
-  #     expect(parsed_response.first['subclass']).to eq('Berserker')
-  #     expect(parsed_response.length).to eq(2)
-  #   end
-  #
-  #   it 'returns an array of subclass levels regardless of case' do
-  #     load_barbarians
-  #     get '/v1/classes/barbarian/beRseRKEr'
-  #
-  #     expect(response.status).to eq(200)
-  #     expect(parsed_response.first['subclass']).to eq('Berserker')
-  #     expect(parsed_response.length).to eq(2)
-  #   end
-  #
-  #   it 'returns an array of subclass levels when using friendly urls' do
-  #     load_barbarians
-  #     get '/v1/classes/barbarian/totem-warrior'
-  #
-  #     expect(response.status).to eq(200)
-  #     expect(parsed_response.first['subclass']).to eq('Totem Warrior')
-  #     expect(parsed_response.length).to eq(1)
-  #   end
-  # end
-  #
+
+  describe 'GET /v1/classes/barbarian/:subclass' do
+    it 'returns a 404 with an invalid subclass' do
+      load_barbarians
+      get '/v1/classes/barbarian/rager'
+      expect(response.status).to eq(404)
+      expect(parsed_response['path']).to eq('/v1/classes/barbarian/rager')
+    end
+
+    it 'returns an array of berserker subclass levels' do
+      load_barbarians
+      get '/v1/classes/barbarian/Berserker'
+
+      expect(response.status).to eq(200)
+      expect(parsed_response.first['subclass']).to eq('Berserker')
+      expect(parsed_response.length).to eq(2)
+    end
+
+    it 'returns an array of subclass levels regardless of case' do
+      load_barbarians
+      get '/v1/classes/barbarian/beRseRKEr'
+
+      expect(response.status).to eq(200)
+      expect(parsed_response.first['subclass']).to eq('Berserker')
+      expect(parsed_response.length).to eq(2)
+    end
+
+    it 'returns an array of subclass levels when using friendly urls' do
+      load_barbarians
+      get '/v1/classes/barbarian/totem-warrior'
+
+      expect(response.status).to eq(200)
+      expect(parsed_response.first['subclass']).to eq('Totem Warrior')
+      expect(parsed_response.length).to eq(1)
+    end
+  end
+
   # describe 'GET /v1/classes/barbarian/:subclass/:level' do
   #   it 'returns a 404 with an invalid subclass and level' do
   #     load_barbarians
