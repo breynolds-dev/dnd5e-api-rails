917bfdaa724ba7dc51ede91869b975e8ce728279
arturusfury <bri.reynolds@gmail.com>
2016-12-25 20:54:08 -0600

    updated our serializer to allow our tests to pass with the associated responses for saving throws and primary abilities

--- app/serializers/class_name_serializer.rb
+++ app/serializers/class_name_serializer.rb
@@ -1,10 +1,32 @@
 class ClassNameSerializer < RouteSerializer
-  attributes :id, :name, :description, :creating_a, :quick_build,
-             :url, :subclasses, :armor_proficiencies, :weapon_proficiencies,
-             :tools
+  attributes :id, :name, :description, :creating_a, :quick_build, :url,
+             :hit_die, :primary_abilities, :saving_throws, :subclasses,
+             :armor_proficiencies, :weapon_proficiencies, :tools
 
   def description
     object.desc.split('/n/r')
+  end
+
+  def hit_die
+    "d#{object.hit_die}"
+  end
+
+  def primary_abilities
+    object.primary_abilities.collect do |ability|
+      {
+        name: ability.name,
+        url: "#{root_url}/abilities/#{make_params(ability.name)}"
+      }
+    end
+  end
+
+  def saving_throws
+    object.saving_throws.collect do |ability|
+      {
+        name: ability.name,
+        url: "#{root_url}/abilities/#{make_params(ability.name)}"
+      }
+    end
   end
 
   def url
@@ -18,5 +40,17 @@ class ClassNameSerializer < RouteSerializer
         url: "#{root_url}/classes/#{make_params(object.name)}/#{make_params(subclass)}"
       }
     end
+  end
+
+  def armor_proficiencies
+    object.armor_proficiencies.split(',')
+  end
+
+  def weapon_proficiencies
+    object.weapon_proficiencies.split(',')
+  end
+
+  def tools
+    object.tools.split(',')
   end
 end
