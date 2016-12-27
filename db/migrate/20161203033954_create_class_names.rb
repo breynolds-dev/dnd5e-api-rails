cdc355c2738c14d8b61ee764d27497fb1cd259ee
arturusfury <bri.reynolds@gmail.com>
2016-12-27 09:07:26 -0600

    updated our class name migration to include a short description of the class, changed desc to description and included two subheadings for additional class text

--- db/migrate/20161203033954_create_class_names.rb
+++ db/migrate/20161203033954_create_class_names.rb
@@ -2,13 +2,18 @@ class CreateClassNames < ActiveRecord::Migration[5.0]
   def change
     create_table :class_names do |t|
       t.string :name, null: false
-      t.string :desc, default: ''
+      t.string :short_description, default: ''
+      t.string :description, default: ''
+      t.string :subheading_one, default: ''
+      t.string :subheading_two, default: ''
+      t.string :description, default: ''
       t.string :creating_a, default: ''
       t.string :quick_build, default: ''
       t.integer :hit_die, null: false
       t.string :armor_proficiencies, default: ''
       t.string :weapon_proficiencies, default: ''
       t.string :tools, default: ''
+      t.string :skill_choice, default: ''
     end
   end
 end
