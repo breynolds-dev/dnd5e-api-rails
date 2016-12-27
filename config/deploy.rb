84960ed05ae09decf624a5e9aaffd4e3e4661399
arturusfury <bri.reynolds@gmail.com>
2016-12-27 11:24:04 -0600

    uncommented a line that lets us pick a branch to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'dnd5e-api'
 set :repo_url, 'git@github.com:breynolds-dev/dnd5e-api.git'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/www/5e-api.com'
