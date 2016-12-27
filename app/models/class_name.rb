4312b854bc4ae4abc0a3e8b418df74d3b47cae16
arturusfury <bri.reynolds@gmail.com>
2016-12-27 11:04:33 -0600

    changed to use class_skill_options instead of just skills to better represent what the skills are there for

--- app/models/class_name.rb
+++ app/models/class_name.rb
@@ -4,7 +4,7 @@ class ClassName < ApplicationRecord
   has_many :class_skills
   has_many :class_saving_throws
   has_many :class_primary_abilities
-  has_many :skills, through: :class_skills
+  has_many :class_skill_options, through: :class_skills, source: 'skill'
   has_many :saving_throws, through: :class_saving_throws, source: 'ability'
   has_many :primary_abilities, through: :class_primary_abilities, source: 'ability'
 
