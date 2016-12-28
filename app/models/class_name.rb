3b089680dffba73988144c380bfd954fc4e851fc
arturusfury <bri.reynolds@gmail.com>
2016-12-28 14:56:31 -0600

    updated our model with a few conditional check methods and our serializer to improve our response to the classes index action

--- app/models/class_name.rb
+++ app/models/class_name.rb
@@ -8,4 +8,12 @@ class ClassName < ApplicationRecord
   has_many :class_skill_options, through: :class_skills, source: 'skill'
   has_many :saving_throws, through: :class_saving_throws, source: 'ability'
   has_many :primary_abilities, through: :class_primary_abilities, source: 'ability'
+
+  def self.valid_class?(name)
+    all.collect { |cls| cls.name.downcase }.include?(name.downcase)
+  end
+
+  def valid_subclass?(name)
+    subclasses.collect { |subcls| subcls.name.downcase }.include?(name.downcase)
+  end
 end
--- app/serializers/indexes/class_index_serializer.rb
+++ app/serializers/indexes/class_index_serializer.rb
@@ -1,16 +1,69 @@
 class Indexes::ClassIndexSerializer < RouteSerializer
-  attributes :name, :url, :subclasses
+  attributes :type, :class_options, :links
+
+  def type
+    'classes'
+  end
+
+  def class_options
+    list = {}
+    object.each do |cls|
+      list.store(make_params(cls.name), make_class_entry(cls))
+    end
+    list
+  end
+
+  def links
+    links = {}
+    links.store('self', make_class_index_link)
+    links.store('classes', classes_links)
+    links.store('subclasses', subclasses_links)
+    links
+  end
+
+  private
+
+  def make_class_entry(class_name)
+    {
+      url: make_class_link(class_name),
+      short_description: class_name.short_description,
+      levels: make_class_levels_link(class_name),
+      subclass_index: make_subclass_index_link(class_name),
+      subclasses: make_subclasses_entry(class_name)
+    }
+  end
+
+  def make_subclasses_entry(class_name)
+    list = {}
+    class_name.subclasses.each do |subclass|
+      unless subclass.name == class_name.name
+        list.store(make_params(subclass.name), make_subclass_link(subclass))
+      end
+    end
+    list
+  end
 
   def url
     make_class_link(object)
   end
 
-  def subclasses
-    subclass_list = {}
-    object.subclasses.drop(1).collect do |subclass|
-      subclass_list.store(make_params(subclass.name),
-                          make_subclass_link(subclass))
+  def classes_links
+    links = {}
+    object.all.each do |cls|
+      links.store(make_params(cls.name), url)
     end
-    subclass_list
+    links
+  end
+
+  def subclasses_links
+    links = {}
+    object.all.each do |cls|
+      cls.subclasses.each do |subcls|
+        unless cls.name == subcls.name
+          links.store(make_params(subcls.name), make_subclass_link(subcls))
+        end
+      end
+    end
+    links
   end
 end
