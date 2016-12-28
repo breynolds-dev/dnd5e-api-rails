884666c8ab4bf8fc07766ad25a68b7d21172ebf0
arturusfury <bri.reynolds@gmail.com>
2016-12-28 14:55:38 -0600

    updated our index serializer and commented out all of the other actions for the controller as we move things around and clean them up

--- app/controllers/api/v1/classes/class_names_controller.rb
+++ app/controllers/api/v1/classes/class_names_controller.rb
@@ -8,75 +8,75 @@ class API::V1::Classes::ClassNamesController < ApplicationController
       resource_not_found('classes', '')
     else
       render json: classes,
-             each_serializer: Indexes::ClassIndexSerializer
+             serializer: Indexes::ClassIndexSerializer
     end
   end
 
-  def class_index
-    class_name = ClassName.load_class_index(params[:class])
-
-    if class_name.nil?
-      resource_not_found('classes', params[:class])
-    else
-      render json: class_name,
-             serializer: ClassNameSerializer
-    end
-  end
-
-  def class_levels_index
-    class_name = ClassName.load_class_levels_index(params[:class])
-
-    if class_name.nil?
-      resource_not_found('classes', "#{params[:class]}/levels")
-    else
-      render json: class_name,
-             serializer: Indexes::ClassLevelIndexSerializer
-    end
-  end
-
-  def class_subclass_index
-    subclass = ClassName.load_subclass_index(params[:class])
-
-    if subclass.nil?
-      resource_not_found('classes', "#{params[:class]}/subclasses")
-    else
-      render json: subclass,
-             each_serializer: Indexes::ClassSubclassIndexSerializer
-    end
-  end
-
-  def class_subclass_levels_index
-    if number?(params[:subclass])
-      level = Level.find_level(params[:class], params[:subclass])
-
-      if level.nil? || params[:subclass].to_i > 2
-        resource_not_found('classes', "#{params[:class]}/#{params[:subclass]}")
-      else
-        render json: level,
-               serializer: ClassDetailSerializer
-      end
-    else
-      subclass = ClassName.load_subclass_levels_index(params[:class], params[:subclass])
-
-      if subclass.nil?
-        resource_not_found('classes', "#{params[:class]}/#{params[:subclass]}")
-      else
-        render json: subclass,
-               each_serializer: Indexes::ClassSubclassLevelIndexSerializer
-      end
-    end
-  end
-
-  def class_levels_show
-    levels = ClassName.load_class_levels(params[:class], params[:level])
-
-    if levels.nil?
-      resource_not_found('classes', "#{params[:class]}/levels/#{params[:level]}")
-    else
-      render json: levels,
-             each_serializer: ClassLevelsSerializer
-    end
-  end
+  # def class_index
+  #   class_name = ClassName.load_class_index(params[:class])
+  #
+  #   if class_name.nil?
+  #     resource_not_found('classes', params[:class])
+  #   else
+  #     render json: class_name,
+  #            serializer: ClassNameSerializer
+  #   end
+  # end
+  #
+  # def class_levels_index
+  #   class_name = ClassName.load_class_levels_index(params[:class])
+  #
+  #   if class_name.nil?
+  #     resource_not_found('classes', "#{params[:class]}/levels")
+  #   else
+  #     render json: class_name,
+  #            serializer: Indexes::ClassLevelIndexSerializer
+  #   end
+  # end
+  #
+  # def class_subclass_index
+  #   subclass = ClassName.load_subclass_index(params[:class])
+  #
+  #   if subclass.nil?
+  #     resource_not_found('classes', "#{params[:class]}/subclasses")
+  #   else
+  #     render json: subclass,
+  #            each_serializer: Indexes::ClassSubclassIndexSerializer
+  #   end
+  # end
+  #
+  # def class_subclass_levels_index
+  #   if number?(params[:subclass])
+  #     level = Level.find_level(params[:class], params[:subclass])
+  #
+  #     if level.nil? || params[:subclass].to_i > 2
+  #       resource_not_found('classes', "#{params[:class]}/#{params[:subclass]}")
+  #     else
+  #       render json: level,
+  #              serializer: ClassDetailSerializer
+  #     end
+  #   else
+  #     subclass = ClassName.load_subclass_levels_index(params[:class], params[:subclass])
+  #
+  #     if subclass.nil?
+  #       resource_not_found('classes', "#{params[:class]}/#{params[:subclass]}")
+  #     else
+  #       render json: subclass,
+  #              each_serializer: Indexes::ClassSubclassLevelIndexSerializer
+  #     end
+  #   end
+  # end
+  #
+  # def class_levels_show
+  #   levels = ClassName.load_class_levels(params[:class], params[:level])
+  #
+  #   if levels.nil?
+  #     resource_not_found('classes', "#{params[:class]}/levels/#{params[:level]}")
+  #   else
+  #     render json: levels,
+  #            each_serializer: ClassLevelsSerializer
+  #   end
+  # end
 
   def show
     subclass = ClassName.show_subclass_entry(params[:class], params[:subclass], params[:level])
