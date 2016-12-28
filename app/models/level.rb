d438a294ec0ade3194b62cd8177f22524caa235a
arturusfury <bri.reynolds@gmail.com>
2016-12-28 16:33:45 -0600

    added our find_level method to get us a level object from our database

--- app/models/level.rb
+++ app/models/level.rb
@@ -6,4 +6,14 @@ class Level < ApplicationRecord
     cls = ClassName.find_by('lower(name) = ?', make_readable(name.downcase))
     cls.levels.where(number: level) unless cls.nil?
   end
+
+  def self.find_level(name, subclass, level)
+    cls = ClassName.find_by('lower(name) = ?', make_readable(name.downcase))
+    unless cls.nil?
+      subcls = cls.subclasses.find_by('lower(name) = ?',
+                                      make_readable(subclass.downcase))
+
+      subcls.levels.find_by(number: level) unless subcls.nil?
+    end
+  end
 end
