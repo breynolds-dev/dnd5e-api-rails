623a74924486042df72c302c0b4eca4653298a99
arturusfury <bri.reynolds@gmail.com>
2016-12-29 09:31:35 -0600

    added our docs html page, included some scrollspy js code for our table of contents and changed some css styling to better fit our color scheme thusfar

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,11 @@
 
     Materialize.updateTextFields();
 
+    $('.scrollspy').scrollSpy();
+    $('.toc-wrapper').pushpin({
+      top: 212
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
@@ -30,22 +35,22 @@ if (!library)
    var library = {};
 
 library.json = {
-   replacer: function(match, pIndent, pKey, pVal, pEnd) {
-      var key = '<span class=json-key>';
-      var val = '<span class=json-value>';
-      var str = '<span class=json-string>';
-      var r = pIndent || '';
-      if (pKey)
-         r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
-      if (pVal)
-         r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
-      return r + (pEnd || '');
-      },
-   prettyPrint: function(obj) {
-      var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
-      return JSON.stringify(obj, null, 3)
-         .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
-         .replace(/</g, '&lt;').replace(/>/g, '&gt;')
-         .replace(jsonLine, library.json.replacer);
-      }
-   };
+  replacer: function(match, pIndent, pKey, pVal, pEnd) {
+    var key = '<span class=json-key>';
+    var val = '<span class=json-value>';
+    var str = '<span class=json-string>';
+    var r = pIndent || '';
+    if (pKey)
+      r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+    if (pVal)
+      r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
+    return r + (pEnd || '');
+  },
+  prettyPrint: function(obj) {
+    var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+    return JSON.stringify(obj, null, 3)
+      .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
+      .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+      .replace(jsonLine, library.json.replacer);
+  }
+};
