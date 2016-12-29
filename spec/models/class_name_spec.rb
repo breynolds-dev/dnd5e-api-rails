28d5477373123073f73d26c7392d08190335d5e6
arturusfury <bri.reynolds@gmail.com>
2016-12-28 20:27:37 -0600

    changed our model test factory since we no longer have a bard factory to use

--- spec/models/class_name_spec.rb
+++ spec/models/class_name_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ClassName, type: :model do
   let(:barbarian) { FactoryGirl.create :barbarian }
-  let(:bard) { FactoryGirl.create :bard }
+  let(:fighter) { FactoryGirl.create :fighter }
 
   it 'should allow the object to be created' do
     expect(barbarian).to be_present
-    expect(bard).to be_present
+    expect(fighter).to be_present
   end
 
   it { should have_many(:levels) }
