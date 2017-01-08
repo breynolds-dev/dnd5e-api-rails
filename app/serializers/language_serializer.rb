c22e6e8cafaf865f0f494964aac91c30035d711a
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2017-01-08 15:44:24 -0600

    partially add 'inherant users' of languages - tests probably still failing

--- app/serializers/language_serializer.rb
+++ app/serializers/language_serializer.rb
@@ -1,15 +1,22 @@
 class LanguageSerializer < RouteSerializer
-  attributes :id, :name, :script, :language_speakers, :links
+  attributes :id, :name, :script, :native_speakers, :links
 
   def links
     {
       self: "#{root_url}/languages/#{make_params(object.name)}",
     }
   end
-  
-  def language_speakers
-    {
-      known_speakers: "#{object.races}"
-    }
+
+  def native_speakers
+    object.races.map do |race|
+      {
+        race: race_name(race),
+        url: "#{root_url}/races/#{make_params(race_name(race))}"
+      }
+    end
+  end
+
+  def race_name(race)
+    race.subrace ? race.subrace : race.name
   end
 end
