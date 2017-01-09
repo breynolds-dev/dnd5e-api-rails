6dc3e6c0ffa77425460bfa8c464d49b41d439382
arturusfury <bri.reynolds@gmail.com>
2017-01-09 12:22:28 -0600

    updated our seeds file name to match the rest, resolved a minor issue where it was looking for name instead of subrace

---
+++ db/seeds/racial_ability_bonuses.rb
@@ -0,0 +1,251 @@
+# RacialAbilityBonus.create(
+# race: Race.find_by(subrace: 'Forest Gnome'),
+# ability: Ability.find_by(name: 'Intelligence'),
+# bonus: 2
+# )
+# RacialAbilityBonus.create(
+# race: Race.find_by(subrace: 'Forest Gnome'),
+# ability: Ability.find_by(name: 'Dexterity'),
+# bonus: 1
+# )
+
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Dragonborn'),
+  ability: Ability.find_by(name: 'Strength'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Dragonborn'),
+  ability: Ability.find_by(name: 'Charisma'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Mountain Dwarf'),
+  ability: Ability.find_by(name: 'Strength'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Mountain Dwarf'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 2
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Hill Dwarf'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Hill Dwarf'),
+  ability: Ability.find_by(name: 'Wisdom'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Duergar'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Duergar'),
+  ability: Ability.find_by(name: 'Strength'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'High Elf'),
+  ability: Ability.find_by(name: 'Dexterity'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'High Elf'),
+  ability: Ability.find_by(name: 'Intelligence'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Wood Elf'),
+  ability: Ability.find_by(name: 'Dexterity'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Wood Elf'),
+  ability: Ability.find_by(name: 'Wisdom'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Dark Elf'),
+  ability: Ability.find_by(name: 'Dexterity'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Dark Elf'),
+  ability: Ability.find_by(name: 'Charisma'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Rock Gnome'),
+  ability: Ability.find_by(name: 'Intelligence'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Rock Gnome'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 1
+)
+
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Deep Gnome'),
+  ability: Ability.find_by(name: 'Intelligence'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Deep Gnome'),
+  ability: Ability.find_by(name: 'Dexterity'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Half Orc'),
+  ability: Ability.find_by(name: 'Strength'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Half Orc'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Lightfoot Halfling'),
+  ability: Ability.find_by(name: 'Dexterity'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Lightfoot Halfling'),
+  ability: Ability.find_by(name: 'Charisma'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Stout Halfling'),
+  ability: Ability.find_by(name: 'Dexterity'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Stout Halfling'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Ghostwise Halfling'),
+  ability: Ability.find_by(name: 'Dexterity'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Ghostwise Halfling'),
+  ability: Ability.find_by(name: 'Wisdom'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Tiefling'),
+  ability: Ability.find_by(name: 'Charisma'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Tiefling'),
+  ability: Ability.find_by(name: 'Intelligence'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Human'),
+  ability: Ability.find_by(name: 'Charisma'),
+  bonus: 1
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Human'),
+  ability: Ability.find_by(name: 'Intelligence'),
+  bonus: 1
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Human'),
+  ability: Ability.find_by(name: 'Wisdom'),
+  bonus: 1
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Human'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 1
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Human'),
+  ability: Ability.find_by(name: 'Strength'),
+  bonus: 1
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Human'),
+  ability: Ability.find_by(name: 'Dexterity'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Goliath'),
+  ability: Ability.find_by(name: 'Strength'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(name: 'Goliath'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Fire Gensai'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Fire Gensai'),
+  ability: Ability.find_by(name: 'Intelligence'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Air Gensai'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Air Gensai'),
+  ability: Ability.find_by(name: 'Dexterity'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Earth Gensai'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Earth Gensai'),
+  ability: Ability.find_by(name: 'Strength'),
+  bonus: 1
+)
+
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Water Gensai'),
+  ability: Ability.find_by(name: 'Constitution'),
+  bonus: 2
+)
+RacialAbilityBonus.create(
+  race: Race.find_by(subrace: 'Water Gensai'),
+  ability: Ability.find_by(name: 'Wisdom'),
+  bonus: 1
+)
