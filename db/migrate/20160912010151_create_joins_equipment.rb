246c28913f1ef849880280cbc1559d24e696d3ff
arturusfury <bri.reynolds@gmail.com>
2017-01-16 18:42:01 -0600

    removed a joins equipment table that was not tied to any models yet, this will be reimplemented later on

--- db/migrate/20160912010151_create_joins_equipment.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinsEquipment < ActiveRecord::Migration[5.0]
-  def change
-    create_table :joins_equipment do |t|
-      t.integer :race_id, null: false
-      t.integer :equipment_id, null: false
-    end
-  end
-end
