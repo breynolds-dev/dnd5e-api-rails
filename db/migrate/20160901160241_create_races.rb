8e45d6dab73bdb641b0dc85796e2b344f95cc704
arturusfury <bri.reynolds@gmail.com>
2017-01-23 15:36:51 -0600

    updated our races database to hold an alignment value, added the aarakocra race information

--- db/migrate/20160901160241_create_races.rb
+++ db/migrate/20160901160241_create_races.rb
@@ -9,6 +9,7 @@ class CreateRaces < ActiveRecord::Migration[5.0]
       t.integer :min_age, default: 0
       t.integer :max_age, default: 0
       t.string :age_description, default: ''
+      t.string :alignment, default: ''
       t.string :size, default: ''
       t.string :size_description, default: ''
       t.integer :min_height, default: 0
