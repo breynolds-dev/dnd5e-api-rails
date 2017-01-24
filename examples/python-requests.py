1377cce7b97335cc178556f19b55558339839d4f
Dustin Rowland <lee.rowland@gmail.com>
2017-01-23 22:16:58 -0600

    Removed repetitive code
    I kept calling .json() methods when the data should be called as JSON the first time I pull it.

--- examples/python-requests.py
+++ examples/python-requests.py
@@ -14,41 +14,41 @@ import requests    # requires pip installation
 API_URL = 'http://5e-api.com/v1/'
 
 # Grab JSON data
-data = requests.get(API_URL + 'classes')
-data1 = requests.get(API_URL + 'races')
-data2 = requests.get(API_URL + 'abilities')
+data = requests.get(API_URL + 'classes').json()
+data1 = requests.get(API_URL + 'races').json()
+data2 = requests.get(API_URL + 'abilities').json()
 
 # List of classes
 class_dict = {}
 
-for index, classes in enumerate(data.json()['class_options']):
+for index, classes in enumerate(data['class_options']):
     class_dict[index] = classes
     print('{}. {} - {}'.format(index, classes,
-          data.json()['class_options'][classes]['short_description']))
+          data['class_options'][classes]['short_description']))
 
 class_choice = input('\nSelect class: ')
 
 class_choice = class_dict[int(class_choice)]
 
-print(data.json()['class_options'][class_choice])
-print(requests.get(data.json()['class_options'][class_choice]['url']).json())
+print(data['class_options'][class_choice])
+print(requests.get(data['class_options'][class_choice]['url']).json())
 
 print('*' * 10)
 
 # List of races
 race_dict = {}
 
-for index, races in enumerate(data1.json()):
+for index, races in enumerate(data1):
     race_dict[index] = races
     print('{}. {}'.format(index, races['name']))
 
 race_choice = int(input('\nSelect race: '))
 
-print(data1.json()[race_choice])
-print(requests.get(data1.json()[race_choice]['url']).json())
+print(data1[race_choice])
+print(requests.get(data1[race_choice]['url']).json())
 
 print('*' * 10)
 
 # List of abilities
-for ability in data2.json():
+for ability in data2:
     print(ability['name'])
