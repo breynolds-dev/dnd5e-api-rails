c14397b419b2aee5826c3569504777038e351da9
Dustin Rowland <lee.rowland@gmail.com>
2017-01-23 22:12:04 -0600

    Just scribbling some python
    Just added some code as a Python example to pull the API using Requests module.

---
+++ examples/python-requests.py
@@ -0,0 +1,54 @@
+#! python3
+# -*- coding: utf-8 -*-
+
+"""
+5e-api.com DND Character Generator.
+
+Using the Dungeons and Dragons 5e API published on github by BReynolds-dev, we
+can script REST API pulls of information for a character builder or reference
+program.
+"""
+import requests    # requires pip installation
+
+''' Constant Definitions '''
+API_URL = 'http://5e-api.com/v1/'
+
+# Grab JSON data
+data = requests.get(API_URL + 'classes')
+data1 = requests.get(API_URL + 'races')
+data2 = requests.get(API_URL + 'abilities')
+
+# List of classes
+class_dict = {}
+
+for index, classes in enumerate(data.json()['class_options']):
+    class_dict[index] = classes
+    print('{}. {} - {}'.format(index, classes,
+          data.json()['class_options'][classes]['short_description']))
+
+class_choice = input('\nSelect class: ')
+
+class_choice = class_dict[int(class_choice)]
+
+print(data.json()['class_options'][class_choice])
+print(requests.get(data.json()['class_options'][class_choice]['url']).json())
+
+print('*' * 10)
+
+# List of races
+race_dict = {}
+
+for index, races in enumerate(data1.json()):
+    race_dict[index] = races
+    print('{}. {}'.format(index, races['name']))
+
+race_choice = int(input('\nSelect race: '))
+
+print(data1.json()[race_choice])
+print(requests.get(data1.json()[race_choice]['url']).json())
+
+print('*' * 10)
+
+# List of abilities
+for ability in data2.json():
+    print(ability['name'])
